1b1947ba7e2ab8eca59017df693123dd6702f8df
LeoConQui <con18797@uvg.edu.gt>
2023-02-20 16:43:56 -0600

    Agregar LecturaTest

---
+++ LecturaTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+
+public class LecturaTest {
+
+
+    @Test 
+    public void TestLectura(){
+        assertEquals(" 12*3^", Lectura.In2Post("(1*2)^3"));
+    }
+
+    
+
+}
